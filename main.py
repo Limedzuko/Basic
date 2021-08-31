188087c80211c38bdd81687f9f7df29ebaf7cbef
Limedzuko <87378325+Limedzuko@users.noreply.github.com>
2021-08-31 13:13:09 +0530

    Super Basic Bot  Initialization- no commands
    Get a token at https://discord.com/developers by making a new application and build a bot.

---
+++ main.py
@@ -0,0 +1,8 @@
+import discord #importing the discord module
+from discord.ext import commands #importing commands
+
+bot = commands.Bot(
+command_prefix'!'
+)#definiting our bot variable which we use a lot later on, and giving it its command prefix
+
+bot.run('BOT TOKEN') 
